e72d114090cfd35f236d974a17a56b337218daca
aldowjy <aldowjy12@gmail.com>
2020-07-31 00:21:59 +0700

    Add Front Screen, Barcode Scanner

---
+++ androidAppToko/app/src/main/java/com/example/inventaristoko/testVolley.java
@@ -0,0 +1,13 @@
+package com.example.inventaristoko;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class testVolley extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_volley);
+    }
+}
