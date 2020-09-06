e15fb91c240184be317f283d64915979e3c46702
aldowjy <aldowjy12@gmail.com>
2020-09-07 00:34:10 +0700

    Fixing File Meja

---
+++ androidAppToko/app/src/main/java/com/example/inventaristoko/Screens/Meja/MejaDetailActivity.java
@@ -0,0 +1,113 @@
+package com.example.inventaristoko.Screens.Meja;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.inventaristoko.R;
+import com.example.inventaristoko.Utils.CommonUtils;
+import com.example.inventaristoko.Utils.MyConstants;
+import com.example.inventaristoko.Utils.VolleyAPI;
+import com.example.inventaristoko.Utils.VolleyCallback;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+public class MejaDetailActivity extends AppCompatActivity {
+    private Button btnDelete, btnEdit;
+    private TextView tvMejaId;
+    private ImageView ivQrCode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meja_detail);
+
+        getSupportActionBar().setTitle(R.string.label_detil_meja);
+
+        tvMejaId = findViewById(R.id.labelValueNamaMeja);
+
+        ivQrCode = findViewById(R.id.ivQrCode);
+
+        btnDelete = findViewById(R.id.buttonDelete);
+        btnEdit = findViewById(R.id.buttonEdit);
+
+        Bundle bundle = getIntent().getExtras();
+        tvMejaId.setText(bundle.getString("userId"));
+
+        String text = tvMejaId.getText().toString();
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,150,150);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            ivQrCode.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                AlertDialog.Builder builder = new AlertDialog.Builder(MejaDetailActivity.this);
+//                builder.setMessage("Anda Yakin Ingin Menghapus Data ini?");
+//                builder.setCancelable(false);
+//                builder.setPositiveButton("Iya", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        CommonUtils.showLoading(MejaDetailActivity.this);
+//                        VolleyAPI volleyAPI = new VolleyAPI(MejaDetailActivity.this);
+//                        Map<String, String> params = new HashMap<>();
+//                        params.put("user_id", tvMejaId.getText().toString());
+//
+//                        volleyAPI.putRequest("deleteMeja", params, new VolleyCallback() {
+//                            @Override
+//                            public void onSuccessResponse(String result) {
+//                                try {
+//                                    JSONObject resultJSON = new JSONObject(result);
+//                                    Intent myIntent = new Intent(getApplicationContext(), MejaActivity.class);
+//                                    startActivityForResult(myIntent, 0);
+//                                    Toast.makeText(getApplicationContext(), resultJSON.getString("message"), Toast.LENGTH_SHORT).show();
+//                                } catch (JSONException e) {
+//                                    e.printStackTrace();
+//                                }
+//                            }
+//                        });
+//                    }
+//                });
+//                builder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                    }
+//                });
+//                builder.show();
+                Toast.makeText(getApplicationContext(), "Delete Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), MejaEntryActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("screenState", MyConstants.EDIT_MEJA);
+                bundle.putString("userId", tvMejaId.getText().toString());
+                intent.putExtras(bundle);
+                v.getContext().startActivity(intent);
+            }
+        });
+    }
+}
---
+++ androidAppToko/app/src/main/java/com/example/inventaristoko/Screens/Meja/MejaEntryActivity.java
@@ -0,0 +1,175 @@
+package com.example.inventaristoko.Screens.Meja;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ImageView;
+
+import com.example.inventaristoko.R;
+import com.example.inventaristoko.Utils.CommonUtils;
+import com.example.inventaristoko.Utils.MyConstants;
+import com.example.inventaristoko.Utils.VolleyAPI;
+import com.example.inventaristoko.Utils.VolleyCallback;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.graphics.Bitmap;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+public class MejaEntryActivity extends AppCompatActivity {
+    private EditText etMejaId;
+    private ImageView ivQrCode;
+    private Button btnGenerate, btnSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meja_entry);
+
+        Bundle bundle = getIntent().getExtras();
+        String screenState = bundle.getString("screenState");
+
+        etMejaId = findViewById(R.id.inputMejaId);
+        ivQrCode = findViewById(R.id.ivQrCode);
+
+        if(screenState.equals(MyConstants.EDIT_MEJA)) {
+            getSupportActionBar().setTitle(R.string.label_edit_meja);
+            etMejaId.setText(bundle.getString("userId"));
+
+            try {
+                String text = etMejaId.getText().toString();
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,150,150);
+                BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                ivQrCode.setImageBitmap(bitmap);
+            } catch (WriterException e) {
+                e.printStackTrace();
+            }
+        } else {
+            getSupportActionBar().setTitle(R.string.label_add_meja);
+        }
+
+        btnGenerate = findViewById(R.id.buttonGenerate);
+        btnGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String text = etMejaId.getText().toString();
+                    MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                    BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,150,150);
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                    ivQrCode.setImageBitmap(bitmap);
+                    hideKeyboard(v);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        btnSubmit = findViewById(R.id.buttonSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                AlertDialog.Builder builder = new AlertDialog.Builder(MejaEntryActivity.this);
+//                builder.setMessage("Anda Yakin Ingin Kirim Data ini?");
+//                builder.setCancelable(false);
+//                builder.setPositiveButton("Iya", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        CommonUtils.showLoading(MejaEntryActivity.this);
+//                        VolleyAPI volleyAPI = new VolleyAPI(MejaEntryActivity.this);
+//                        Map<String, String> params = new HashMap<>();
+//                        params.put("user_id", etMejaId.getText().toString());
+//
+//                        if (screenState.equals(MyConstants.EDIT_PENGGUNA)) {
+//                            params.put("user_id_old", bundle.getString("userId"));
+//                        }
+//
+//                        try {
+//                            String text = etMejaId.getText().toString();
+//                            MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+//                            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,150,150);
+//                            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+//                            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+//                            params.put("gambar_makanan", etMejaId.getText().toString());
+//                        } catch (WriterException e) {
+//                            e.printStackTrace();
+//                        }
+//
+//                        if (screenState.equals(MyConstants.EDIT_PENGGUNA)) {
+//                            volleyAPI.putRequest("updateMeja", params, new VolleyCallback() {
+//                                @Override
+//                                public void onSuccessResponse(String result) {
+//                                    try {
+//                                        JSONObject resultJSON = new JSONObject(result);
+//                                        Intent myIntent = new Intent(getApplicationContext(), MejaActivity.class);
+//                                        startActivityForResult(myIntent, 0);
+//                                        Toast.makeText(getApplicationContext(), resultJSON.getString("message"), Toast.LENGTH_SHORT).show();
+//                                    } catch (JSONException e) {
+//                                        e.printStackTrace();
+//                                    }
+//                                }
+//                            });
+//                        } else if (screenState.equals(MyConstants.ADD_PENGGUNA)) {
+//                            volleyAPI.postRequest("registerMeja", params, new VolleyCallback() {
+//                                @Override
+//                                public void onSuccessResponse(String result) {
+//                                    try {
+//                                        JSONObject resultJSON = new JSONObject(result);
+//                                        Intent myIntent = new Intent(getApplicationContext(), MejaActivity.class);
+//                                        startActivityForResult(myIntent, 0);
+//                                        Toast.makeText(getApplicationContext(), resultJSON.getString("message"), Toast.LENGTH_SHORT).show();
+//                                    } catch (JSONException e) {
+//                                        e.printStackTrace();
+//                                    }
+//                                }
+//                            });
+//                        }
+//                    }
+//                });
+//                builder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                    }
+//                });
+//                builder.show();
+
+                Toast.makeText(getApplicationContext(), "Submit Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        etMejaId.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
+    }
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+}
